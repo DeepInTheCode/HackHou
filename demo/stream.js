1a8b853ef0510e9a6fa43d1067d66191e18420ee
Antony Wu <antony@meshway.com>
2013-05-18 21:44:42 -0500

    post-demo: Tidy up the code a little bit Part 2
    Remove additional references to movies/genre

--- demo/stream.js
+++ demo/stream.js
@@ -1,20 +1,21 @@
 jQuery(document).ready(function($) {
 
-  var genre_template = Mustache.compile($.trim($("#genre_template").html()))
-      ,$genre_container = $('#genre_criteria') 
+  var depts_template = Mustache.compile($.trim($("#depts_template").html()))
+      ,$dept_container = $('#dept_criteria') 
 
   $.each(all_deps, function(i, d){
-    $genre_container.append(genre_template({dept: d}));
+    $dept_container.append(depts_template({dept: d}));
   });
 
   $.each(master_fees, function(i, m){ m.id = i+1; });
   window.mf = FeesFilter(master_fees);
 
-  $('#genre_criteria :checkbox').prop('checked', true);
-  $('#all_genre').on('click', function(e){
-    $('#genre_criteria :checkbox:gt(0)').prop('checked', $(this).is(':checked'));
+  $('#dept_criteria :checkbox').prop('checked', true);
+  $('#all_dept').on('click', function(e){
+    $('#dept_criteria :checkbox:gt(0)').prop('checked', $(this).is(':checked'));
     mf.filter();
   });
+  mf.filter();
 
 });
 
@@ -26,18 +27,18 @@ var FeesFilter = function(data){
   };
   var callbacks = {
     show_search_count: function(result){
-      $('#total_movies').text(result.length);
+      $('#total_fees').text(result.length);
     },
   };
 
   options = {
     filter_criteria: {
-      genre: ['#genre_criteria input:checkbox:gt(0)', 'ResponsibleDepartment']
+      dept: ['#dept_criteria input:checkbox:gt(0)', 'ResponsibleDepartment']
     },
     and_filter_on: true,
     callbacks: callbacks,
     search: {input: '#searchbox'},
   }
 
-  return FilterJS(data, "#movies", view, options);
+  return FilterJS(data, "#fees", view, options);
 }
