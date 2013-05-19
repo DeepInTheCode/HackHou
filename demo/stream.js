075650a68ccabf8066005bfa2649581395baa3de
Antony Wu <antony@meshway.com>
2013-05-18 21:23:32 -0500

    post-demo: Tidy up the code a little bit Part 1
    Project was retrofitted from a movies example into fees demo. So removing some references to movies

--- demo/stream.js
+++ demo/stream.js
@@ -1,32 +1,14 @@
 jQuery(document).ready(function($) {
 
-  var all_deps = [ 'PWE',
-  'Health and Human Services',
-  'Houston Airport System',
-  'City Secretary',
-  'Department of Neighborhoods',
-  'Library',
-  'Administration and Regulatory Affairs',
-  'Finance',
-  'Parks and Recreation',
-  'Police',
-  'Municipal Courts Department',
-  'Solid Waste Management',
-  'Fire',
-  'Planning and Development',
-  'Mayor Office',
-  'City Controller' ];
   var genre_template = Mustache.compile($.trim($("#genre_template").html()))
       ,$genre_container = $('#genre_criteria') 
 
-  $.each(all_deps, function(i, g){
-    $genre_container.append(genre_template({genre: g}));
+  $.each(all_deps, function(i, d){
+    $genre_container.append(genre_template({dept: d}));
   });
 
-  
-
   $.each(master_fees, function(i, m){ m.id = i+1; });
-  window.mf = MovieFilter(master_fees);
+  window.mf = FeesFilter(master_fees);
 
   $('#genre_criteria :checkbox').prop('checked', true);
   $('#all_genre').on('click', function(e){
@@ -36,11 +18,11 @@ jQuery(document).ready(function($) {
 
 });
 
-var MovieFilter = function(data){
+var FeesFilter = function(data){
   var template = Mustache.compile($.trim($("#template").html()));
 
-  var view = function(movie){
-    return template(movie);
+  var view = function(fee){
+    return template(fee);
   };
   var callbacks = {
     show_search_count: function(result){
@@ -54,27 +36,8 @@ var MovieFilter = function(data){
     },
     and_filter_on: true,
     callbacks: callbacks,
-    search: {input: '#searchbox'}/*,
-    streaming: {
-      data_url: 'data/top_movies_data.json', 
-      stream_after: 1,
-      batch_size: 50,
-      before_add: function(data){
-        var offset = this.data.length;
-
-        if (offset == 450) this.clearStreamingTimer();
-        
-        for(var i = 0, l = data.length; i < l; i++)
-          data[i].id = offset + i + 1;
-      },
-      after_add: function(data){
-        var percent = (this.data.length - 250)*100/250;
-        $('#stream_progress').text(percent + '%').attr('style', 'width: '+ percent +'%;');
-        if (percent == 100) $('#stream_progress').parent().fadeOut(1000);
-      }
-    }*/
+    search: {input: '#searchbox'},
   }
 
   return FilterJS(data, "#movies", view, options);
 }
-
